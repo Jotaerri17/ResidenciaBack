71d9c05926b8e100c3c863bb2d10355570833487
Gabriel Lira Figueira <gabriel.figueira@souunit.com.br>
2026-04-22 22:00:22 -0300

    feat(FAC): setup de testes com Jest e Supertest

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
 
 const express = require('express')
 const cors = require('cors')
